632a6fd094b11a9710410cb9eceffd325c5a4b0c
TheVinh26 <maithevinh2723@gmail.com>
2024-11-18 22:48:02 +0700

    add API Cell

--- BE/DLL/obj/Debug/net8.0/DLL.AssemblyInfo.cs
+++ BE/DLL/obj/Debug/net8.0/DLL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DLL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10a8cd526144605b6a3f49210465a41298092d0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+351e2a8c23ce1ca6c4ee689b93c0701b65fed062")]
 [assembly: System.Reflection.AssemblyProductAttribute("DLL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DLL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
